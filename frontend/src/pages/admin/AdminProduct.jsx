1a9a7010e42c81e79384caba3ceab1f6382e5d66
sandhya144 <186sndy@gmail.com>
2026-07-17 23:02:56 +0530

    Admin Product Page Completed

--- frontend/src/pages/admin/AdminProduct.jsx
+++ frontend/src/pages/admin/AdminProduct.jsx
@@ -1,11 +1,334 @@
-import React from 'react'
+import { Input } from "@/components/ui/input";
+import { Edit, Search, Trash2 } from "lucide-react";
+import React, { useState } from "react";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Card } from "@/components/ui/card";
+import { useEffect } from "react";
+import axios from "axios";
+import { useDispatch } from "react-redux";
+import { setProducts } from "@/redux/productSlice";
+import { useSelector } from "react-redux";
+
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
+import ImageUpload from "@/components/ImageUpload";
+import { toast } from "sonner";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+
+
+
+
 
 const AdminProduct = () => {
-  return (
-    <div>
-      AdminProduct
-    </div>
-  )
-}
+  const { products } = useSelector((store) => store.product);
+  const dispatch = useDispatch();
+  const [open, setOpen] = useState(false);
 
-export default AdminProduct
+  const [searchTerm, SetSearchTerm] = useState("")
+  const [sortOrder, setSortOrder] = useState("")
+
+  const [editProduct, setEditProduct] = useState(null);
+  const accessToken = localStorage.getItem("accessToken");
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setEditProduct((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const handleSave = async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+
+    formData.append("productName", editProduct.productName);
+    formData.append("productDesc", editProduct.productDesc);
+    formData.append("productPrice", editProduct.productPrice);
+    formData.append("category", editProduct.category);
+    formData.append("brand", editProduct.brand);
+
+    // add existing images public_ids
+    const existingImages = editProduct.productImg
+      .filter((img) => !(img instanceof File) && img.public_id)
+      .map((img) => img.public_id);
+
+    formData.append("existingImages", JSON.stringify(existingImages));
+
+    // add new files
+    editProduct.productImg
+      .filter((img) => img instanceof File)
+      .forEach((file) => {
+        formData.append("files", file);
+      });
+
+    try {
+      const res = await axios.put(
+        `http://localhost:8000/api/v1/product/update/${editProduct._id}`,
+        formData,
+        {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        },
+      );
+      console.log(res.data.product);
+      if (res.data.success) {
+        toast.success("Product Added Successfully");
+        const updateProducts = products.map((p) =>
+          p._id === editProduct._id ? res.data.product : p,
+        );
+        dispatch(setProducts(updateProducts));
+        setOpen(false);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const deleteProductHandler = async (productId) => {
+    try {
+      const remainingProducts = products.filter(
+        (product) => product._id !== productId,
+      );
+      const res = await axios.delete(
+        `http://localhost:8000/api/v1/product/delete/${productId}`,
+        {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        },
+      );
+      if (res.data.success) {
+        toast.success(res.data.message);
+        dispatch(setproducts(remainingProducts));
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  let filteredProducts = products.filter((product)=> 
+      product.productName.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      product.brand.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      product.category.toLowerCase().includes(searchTerm.toLowerCase())
+  )
+
+  if(sortOrder == 'lowToHigh'){
+    filteredProducts = [...filteredProducts].sort((a,b)=> a.productPrice - b.productPrice)
+  }
+
+  if(sortOrder == 'highToLow'){
+    filteredProducts = [...filteredProducts].sort((a,b)=> b.productPrice - a.productPrice)
+  }
+  
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const res = await axios.get(
+        "http://localhost:8000/api/v1/product/getallproducts",
+      );
+
+      console.log(res.data);
+      dispatch(setProducts(res.data.products));
+    };
+
+    fetchProducts();
+  }, []);
+
+  console.log(products);
+
+  return (
+    <div className="pl-[350px] py-20 pr-20 flex flex-col gap-3 min-h-screen bg-gray-100">
+      <div className="flex justify-between ">
+        <div className="relative bg-white rounded-lg">
+          <Input
+            type="text"
+            placeholder="Search Product..."
+            value={searchTerm} 
+            onChange={(e)=>SetSearchTerm(e.target.value)}
+            className="w-[400px] items-center"
+          />
+          <Search className="absolute right-3 top-1/2 -translate-y-1/2 text-gray-500 h-5 w-5" />
+        </div>
+        <Select onValueChange={(value)=>setSortOrder(value)} >
+          <SelectTrigger className="w-[200px] bg-white">
+            <SelectValue placeholder="Sort By Price" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup>
+              <SelectItem value="lowToHigh"> Price: Low to High </SelectItem>
+              <SelectItem value="highToLow"> Price: High to Low </SelectItem>
+            </SelectGroup>
+          </SelectContent>
+        </Select>
+      </div>
+
+      {filteredProducts.map((product, index) => {
+        return (
+          <Card key={index}>
+            <div className="flex items-center justify-between">
+              <div className="flex gap-2 items-center">
+                <img
+                  src={product?.productImg[0].url}
+                  alt=""
+                  className="w-25 h-25"
+                />
+                <h1 className="font-bold w-96 text-gray-700">
+                  {product?.productName}
+                </h1>
+              </div>
+              <h1 className="font-semibold text-gray-800">
+                {product?.productPrice}
+              </h1>
+              <div className="flex gap-3">
+                <Dialog open={open} onOpenChange={setOpen}>
+                  <DialogTrigger asChild>
+                    <Edit
+                      onClick={() => {
+                        (setOpen(true), setEditProduct(product));
+                      }}
+                      className="text-green-500 cursor-pointer"
+                    />
+                  </DialogTrigger>
+
+                  <DialogContent className="w-[85vw] !max-w-6xl h-[90vh] overflow-y-auto">
+                    <DialogHeader>
+                      <DialogTitle>Edit Product</DialogTitle>
+                      <DialogDescription>
+                        Update your product details. Click save when you are
+                        done.
+                      </DialogDescription>
+                    </DialogHeader>
+
+                    {/* Your form goes here */}
+                    <div className="flex flex-col gap-3 ">
+                      <div className=" grid gap-2">
+                        <Label>Product Name</Label>
+                        <Input
+                          type="text"
+                          value={editProduct?.productName}
+                          onChange={handleChange}
+                          name="productName"
+                          placeholder="Eg-Iphone"
+                          required
+                        />
+                      </div>
+                      <div className=" grid gap-2">
+                        <Label>Price</Label>
+                        <Input
+                          type="number"
+                          value={editProduct?.productPrice}
+                          onChange={handleChange}
+                          name="productPrice"
+                          required
+                        />
+                      </div>
+                      <div className="grid grid-cols-2 gap-4">
+                        <div className=" grid gap-2">
+                          <Label>Brand</Label>
+                          <Input
+                            type="text"
+                            value={editProduct?.brand}
+                            onChange={handleChange}
+                            name="brand"
+                            placeholder="Eg-Apple"
+                          />
+                        </div>
+
+                        <div className=" grid gap-2">
+                          <Label>Category</Label>
+                          <Input
+                            type="text"
+                            value={editProduct?.category}
+                            onChange={handleChange}
+                            name="category"
+                            placeholder="Eg-Mobile"
+                            required
+                          />
+                        </div>
+                      </div>
+                      <div className=" grid gap-2">
+                        <Label>Description</Label>
+                      </div>
+                      <Textarea
+                        name="productDesc"
+                        value={editProduct?.productDesc}
+                        onChange={handleChange}
+                        placeholder="Enter brief description of product."
+                      />
+                    </div>
+                    <ImageUpload
+                      productData={editProduct}
+                      setProductData={setEditProduct}
+                    />
+
+                    <DialogFooter>
+                      <DialogClose asChild>
+                        <Button variant="outline">Cancel</Button>
+                      </DialogClose>
+
+                      <Button onClick={handleSave}>Save</Button>
+                    </DialogFooter>
+                  </DialogContent>
+                </Dialog>
+
+                <AlertDialog>
+                  <AlertDialogTrigger render={<Button variant="outline" />}>
+                     <Trash2 className="text-red-500 cursor-pointer" />
+                  </AlertDialogTrigger>
+                  <AlertDialogContent>
+                    <AlertDialogHeader>
+                      <AlertDialogTitle>
+                        Are you absolutely sure?
+                      </AlertDialogTitle>
+                      <AlertDialogDescription>
+                        This action cannot be undone. This will permanently
+                        delete your account and remove your data from our servers.
+                      </AlertDialogDescription>
+                    </AlertDialogHeader>
+                    <AlertDialogFooter>
+                      <AlertDialogCancel>Cancel</AlertDialogCancel>
+                      <AlertDialogAction onClick={()=>deleteProductHandler(product._id)}>Continue</AlertDialogAction>
+                    </AlertDialogFooter>
+                  </AlertDialogContent>
+                </AlertDialog>
+
+              </div>
+            </div>
+          </Card>
+        );
+      })}
+    </div>
+  );
+};
+
+export default AdminProduct;
