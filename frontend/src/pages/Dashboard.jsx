5947c2140692c462a13fbf17a4a7be8356b2561f
sandhya144 <186sndy@gmail.com>
2026-09-04 15:58:08 +0530

    final commit

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -4,22 +4,6 @@ import React from 'react'
 import { Outlet } from "react-router-dom";
 
 
-// const Dashboard = () => {
-//   return (
-//     <div className='flex'>
-
-//       {/* sidebar */}
-//       <Sidebar className='bg-[#F8FAFC]' />
-    
-//       <div className='flex-1'>
-//       <Outlet />
-//       </div>
-
-//       </div>
-//   );
-// };
-
-
 const Dashboard = () => {
   return (
     <div className="min-h-screen">
--- frontend/src/pages/Verify.jsx
+++ frontend/src/pages/Verify.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 const Verify = () => {
   return (
     <div className='relative w-full h-190px overflow-hidden'>
-      <div className='min-h-screen flex items-center justify-center bg-pink-100 px-4'>
+      <div className='min-h-screen flex items-center justify-center bg-[#dff6f8]  px-4'>
         <div className='bg-white p-8 rounded-2xl shadow-lg w-full max-w-md text-center'>
-            <h2 className='text-2xl font-semibold text-green-500 mb-4'> ✅ Check Your Email</h2>
+            <h2 className='text-2xl font-semibold text-[#0EA5B4] mb-4'>🔎 Check Your Email</h2>
             <p className='text-gray-400 text-sm'>
                 We've sent you an email to verify your account. Please check your inbox and click the verification link.
             </p>
