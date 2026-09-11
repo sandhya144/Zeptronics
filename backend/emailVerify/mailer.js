5021a2a16f486763425fc838ed627a9487825d0e
sandhya144 <186sndy@gmail.com>
2026-09-11 16:19:10 +0530

    fix gmail smtp connection

--- backend/emailVerify/mailer.js
+++ backend/emailVerify/mailer.js
@@ -2,7 +2,10 @@ import nodemailer from "nodemailer";
 import "dotenv/config";
 
 const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    requireTLS: true,
 
     auth: {
         user: process.env.MAIL_USER,
